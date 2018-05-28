86e287c1846752735c49f8df4e49cea91479b457
Lenny Kean <lennykean@gmail.com>
2018-05-28 08:58:09 -0500

    Add option to reset collector settings

--- src/MiningMonitor.Web/ClientApp/components/admin/SettingsForm.tsx
+++ src/MiningMonitor.Web/ClientApp/components/admin/SettingsForm.tsx
@@ -17,6 +17,12 @@ export class SettingsForm extends React.Component<Props, { [key: string]: string
         super(props);
         this.state = props.settings || {};
     }
+    public componentWillReceiveProps(props: Props) {
+        if (props === this.props) {
+            return;
+        }
+        this.setState(props.settings);
+    }
     @autobind
     public change(event: React.ChangeEvent<HTMLInputElement>) {
         this.setState({
@@ -31,6 +37,20 @@ export class SettingsForm extends React.Component<Props, { [key: string]: string
         event.preventDefault();
         if (this.props.onSubmit) {
             this.props.onSubmit(this.state);
+        }
+    }
+    @autobind
+    public resetCollector() {
+        event.preventDefault();
+        if (this.props.onSubmit) {
+            this.props.onSubmit({
+                ...this.state,
+                name: null,
+                collectorId: null,
+                isDataCollector: null,
+                serverToken: null,
+                serverUrl: null,
+            });
         }
     }
     public render() {
@@ -53,46 +73,59 @@ export class SettingsForm extends React.Component<Props, { [key: string]: string
                             </FormGroup>
                         </Col>
                     </FormGroup>
-                    <FormGroup row title="Run server in data collector mode">
-                        <Label for="isDataCollector" sm={2}>Data Collector</Label>
-                        <Col sm={{ size: 10 }}>
-                            <FormGroup check>
-                                <Label check>
-                                    <FormInput
-                                        id="isDataCollector"
-                                        name="isDataCollector"
-                                        type="checkbox"
-                                        checked={this.state.isDataCollector === 'true'}
-                                        onChange={this.change}
-                                    /> Enabled
-                            </Label>
-                            </FormGroup>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for="serverUrl" sm={2}>Remote Server URL</Label>
-                        <Col sm={{ size: 10 }}>
-                            <FormInput
-                                id="serverUrl"
-                                name="serverUrl"
-                                type="text"
-                                value={this.state.serverUrl || ''}
-                                onChange={this.change}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for="name" sm={2}>Collector Name</Label>
-                        <Col sm={{ size: 10 }}>
-                            <FormInput
-                                id="name"
-                                name="name"
-                                type="text"
-                                value={this.state.name || ''}
-                                onChange={this.change}
-                            />
-                        </Col>
-                    </FormGroup>
+                    <fieldset disabled={this.props.settings && this.props.settings.isDataCollector === 'true'}>
+                        <FormGroup row title="Run server in data collector mode">
+                            <Label for="isDataCollector" sm={2}>Data Collector</Label>
+                            <Col sm={{ size: 10 }}>
+                                <FormGroup check>
+                                    <Label check>
+                                        <FormInput
+                                            id="isDataCollector"
+                                            name="isDataCollector"
+                                            type="checkbox"
+                                            checked={this.state.isDataCollector === 'true'}
+                                            onChange={this.change}
+                                        /> Enabled
+                                </Label>
+                                </FormGroup>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="serverUrl" sm={2}>Remote Server URL</Label>
+                            <Col sm={{ size: 10 }}>
+                                <FormInput
+                                    id="serverUrl"
+                                    name="serverUrl"
+                                    type="text"
+                                    value={this.state.serverUrl || ''}
+                                    onChange={this.change}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="name" sm={2}>Collector Name</Label>
+                            <Col sm={{ size: 10 }}>
+                                <FormInput
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    value={this.state.name || ''}
+                                    onChange={this.change}
+                                />
+                            </Col>
+                        </FormGroup>
+                    </fieldset>
+                    {this.props.settings && this.props.settings.isDataCollector === 'true' ?
+                        <FormGroup row>
+                            <Col sm={2} />
+                            <Col sm={{ size: 10 }}>
+                                <Button
+                                    outline
+                                    color="info"
+                                    onClick={this.resetCollector}
+                                >Reset Collector Settings</Button>
+                            </Col>
+                        </FormGroup> : null}
                     <Button outline color="info">Save Changes</Button>
                 </Form>
             </>
