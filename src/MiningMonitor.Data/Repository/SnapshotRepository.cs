a96719d3d10007921f3aa9971ac552a692cf7315
Lenny Kean <lennykean@gmail.com>
2018-06-09 18:45:39 -0500

    Add indexes to snapshot dbcollection

--- src/MiningMonitor.Data/Repository/SnapshotRepository.cs
+++ src/MiningMonitor.Data/Repository/SnapshotRepository.cs
@@ -12,11 +12,13 @@ namespace MiningMonitor.Data.Repository
     {
         public SnapshotRepository(LiteCollection<Snapshot> dbCollection) : base(dbCollection)
         {
+            dbCollection.EnsureIndex(s => s.MinerId);
+            dbCollection.EnsureIndex(s => s.SnapshotTime);
         }
 
         public Task<IEnumerable<Snapshot>> GetByMinerAsync(Guid minerId, DateTime? from, DateTime? to)
         {
-            return Task.Run(() => DbCollection.Find(snapshot => minerId == snapshot.MinerId && (from == null || from <= snapshot.SnapshotTime) && (to == null || to >= snapshot.SnapshotTime)));
+            return Task.Run(() => DbCollection.Find(snapshot => snapshot.MinerId == minerId && (from == null || snapshot.SnapshotTime >= from) && (to == null || snapshot.SnapshotTime < to)));
         }
 
         public Task<int> DeleteByMinerAsync(Guid minerId)
