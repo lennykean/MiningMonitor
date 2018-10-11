b4719a7b99e24d18ff873a865002fe03b93d6767
Lenny Kean <lennykean@gmail.com>
2018-10-10 22:10:03 -0500

    Allow data collector to run independently

--- src/MiningMonitor.Workers.DataCollector/Program.cs
+++ src/MiningMonitor.Workers.DataCollector/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using MiningMonitor.Data.MongoDb;
+using MiningMonitor.Service;
 
 namespace MiningMonitor.Workers.DataCollector
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddUserSecrets(Assembly.GetEntryAssembly(), optional: true)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(config => config.AddConsole())
+                .AddMongoRepository(configuration.GetConnectionString("miningmonitor"))
+                .AddMiningMonitorServices()
+                .AddSingleton<DataCollectorWorker>()
+                .BuildServiceProvider();
+            
+            var worker = serviceProvider.GetService<DataCollectorWorker>();
+
+            await worker.DoWorkAsync(CancellationToken.None);
         }
     }
 }
